26d737a9148395ca62d16aa7f454a49df2e0971f
mnul <junk@pixelemotionen.de>
2025-07-28 23:00:31 +0200

    added install and uninstall script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Find all dot files then if the original file exists, create a backup
+# Once backed up to {file}.dtbak symlink the new dotfile in place
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+# Install  vim-addons ZSH and ZSH extras
+#    echo "installing extras"
+#    sudo apt update && sudo apt -y install zsh zsh-syntax-highlighting zsh-autosuggestions
+
+echo "Installed"
+echo "use chsh -s /bin/zsh to switch to ZSH shell"
+
